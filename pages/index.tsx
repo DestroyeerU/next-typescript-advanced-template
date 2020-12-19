c25244bd6f33d2ed7c74cddd52012c7b88647fd2
DestroyeerU <destroyeeru18@gmail.com>
2020-12-19 14:42:28 -0300

    Eslint setup

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,19 @@
-import Head from 'next/head'
+import React from "react";
 
-export default function Home() {
+import Head from "next/head";
+
+const Home: React.FC = () => {
   return (
     <div>
       <Head>
         <title>Homepage</title>
       </Head>
 
-      <main >
-       <h1>Hello World</h1>
+      <main>
+        <h1>Hello World</h1>
       </main>
     </div>
-  )
-}
+  );
+};
+
+export default Home;
